78d6c01dad44edb2facb88455c6d730061743380
Michael Miroshnichenko <michael.miroshnichenko@viaamadeus.com>
2015-10-27 18:01:01 +0200

    DAY-13

--- src/Ibw/JobeetBundle/Controller/DefaultController.php
+++ src/Ibw/JobeetBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace Ibw\JobeetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name = 'Jobeet')
+    public function loginAction(Request $request)
     {
-        return $this->render('IbwJobeetBundle:Default:index.html.twig', array('name' => $name));
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(Security::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(Security::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(Security::AUTHENTICATION_ERROR);
+            $session->remove(Security::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('IbwJobeetBundle:Default:login.html.twig', array(
+            // last username entered by the user
+            'last_username' => $session->get(Security::LAST_USERNAME),
+            'error'         => $error,
+        ));
     }
 }
--- src/Ibw/JobeetBundle/Controller/JobController.php
+++ src/Ibw/JobeetBundle/Controller/JobController.php
@@ -107,7 +107,7 @@ class JobController extends Controller
      * Finds and displays a Job entity.
      *
      */
-    public function showAction($id)
+    public function showAction(Request $request, $id)
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -115,6 +115,22 @@ class JobController extends Controller
 
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find Job entity.');
+        }
+
+        $session = $request->getSession();
+
+        // fetch jobs already stored in the job history
+        $jobs = $session->get('job_history', array());
+
+        // store the job as an array so we can put it in the session and avoid entity serialize errors
+        $job = array('id' => $entity->getId(), 'position' =>$entity->getPosition(), 'company' => $entity->getCompany(), 'companyslug' => $entity->getCompanySlug(), 'locationslug' => $entity->getLocationSlug(), 'positionslug' => $entity->getPositionSlug());
+
+        if (!in_array($job, $jobs)) {
+            // add the current job at the beginning of the array
+            array_unshift($jobs, $job);
+
+            // store the new job history back into the session
+            $session->set('job_history', array_slice($jobs, 0, 3));
         }
 
         $deleteForm = $this->createDeleteForm($id);
@@ -272,6 +288,8 @@ class JobController extends Controller
     {
         $form = $this->createPublishForm($token);
         $form->handleRequest($request);
+
+        $this->get('session')->getFlashBag()->add('notice', 'Your job is now online for 30 days.');
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
